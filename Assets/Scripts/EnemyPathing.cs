5afc12b61b9d357e9e20b84046dfa768b211d852
SomeRussianDude <yellow.mockingbird@gmail.com>
2021-11-28 12:47:12 +0100

    Implemented multiple wave spawning, created a new path, wave config is now set via method, not serialized field

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -6,16 +6,14 @@ using UnityEngine.Serialization;
 public class EnemyPathing : MonoBehaviour
 {
     // Config params
-    [SerializeField] private WaveConfig WaveConfig;
-    [SerializeField] private float moveSpeed = 2f;
-
+    private WaveConfig waveConfig;
     private int waypointIndex = 0;
     private List<Transform> waypoints;
 
     // Start is called before the first frame update
     void Start()
     {
-        waypoints = WaveConfig.GetWaypoints();
+        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
@@ -25,12 +23,16 @@ public class EnemyPathing : MonoBehaviour
         Move();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
     private void Move()
     {
         if (waypointIndex < waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex+1].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.MoveSpeed * Time.deltaTime;
             transform.position = Vector2.MoveTowards
                 (transform.position, targetPosition, movementThisFrame);
 

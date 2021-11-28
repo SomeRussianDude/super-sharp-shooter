b67d57c739b038e506235d6af5a017ab5f2c254e
SomeRussianDude <yellow.mockingbird@gmail.com>
2021-11-28 12:50:11 +0100

    Separate commit for EnemySpawner.. No idea why it didn't go with the previous

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Config params
+    [SerializeField] private List<WaveConfig> waveConfigs;
+    [SerializeField] private int startingWave = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
+    }
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemiesSpawned = 0; enemiesSpawned < waveConfig.NumberOfEnemies; enemiesSpawned++)
+        {
+            var newEnemy = Instantiate(waveConfig.EnemyPrefab, waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.TimeBetweenSpawns);
+        }
+    }
+}

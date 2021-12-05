36460c508ab733180cfc1c39b65bbacae23d466a
SomeRussianDude <yellow.mockingbird@gmail.com>
2021-12-05 15:35:24 +0100

    Enemy bombs spinning implemented

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] private float speedOfSpin = 360f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+    }
+}
